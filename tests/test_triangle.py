aab919fd2bb15782aa72523271d293c1f41b50c9
Mackenzie Blanusa <mackenzie.blanusa@uconn.edu>
2025-08-14 10:58:05 -0800

    Updated files to make sure proper imports included

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,4 +1,9 @@
-def test_hypot(a: float, b: float):
+import numbers
+#import sys
+#sys.path.append('/Users/mackenzie/Documents/Research/PhD/URSSI/mygeopy/mygeopy')
+from mygeopy.triangle import hypot
+
+def test_hypot():
     assert hypot(3,4) == 5
     assert hypot(6,8) == 10
     assert hypot(9,12) == 15
